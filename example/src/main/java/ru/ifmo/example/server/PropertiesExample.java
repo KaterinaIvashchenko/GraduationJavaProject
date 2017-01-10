a06a458cd8ebcc180bce1fc4da6b75cbf4548824
katerina ivashchenko <ivashchenko.ki@gmail.com>
2017-01-11 00:47:03 +0300

    Properties (with PropertiesExample)

--- example/src/main/java/ru/ifmo/example/server/PropertiesExample.java
+++ example/src/main/java/ru/ifmo/example/server/PropertiesExample.java
@@ -7,26 +7,81 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.Map;
 
-/**
- * Created by katerina on 1/8/17.
- */
+/** Created by katerina on 1/8/17. */
 
 public class PropertiesExample {
 
-    public static void main(String[] args) throws URISyntaxException, IOException {
+    public static void main(String[] args) throws URISyntaxException, NullPointerException, IOException {
 
-        /*Server server = Server.start(getClass().getClassLoader().getResource("web-server.properties").getFile());
-        */
-        String infoPage = "http://localhost:" + 8081 + "/info.html";
+        File file = new File(PropertiesExample.class.getClassLoader().getResource("web-server.properties").getFile());
+
+        Server.start(file);
+
+        String successPage = "http://localhost:" + 8081 + "/success";
 
         if (Desktop.isDesktopSupported()) {
-            Desktop.getDesktop().browse(new URI(infoPage));
-        }
-        else
-            System.out.println(">>> Open " + infoPage);
+            Desktop.getDesktop().browse(new URI(successPage));
+        } else
+            System.out.println(">>> Open " + successPage);
 
+    }
+
+    public static class SuccessHandler implements Handler {
+        @Override
+        public void handle(Request request, Response response) throws Exception {
+            // Set correct header
+            StringBuilder sb = new StringBuilder(Http.OK_HEADER);
+
+            // Set doctype
+            sb.append("<!DOCTYPE html>");
+
+            // Write some HTML
+            sb.append("<html><body>");
+
+            sb.append("<p>http://localhost:8081/success").append("<br>");
+            sb.append("<p>Requested address: ").append(request.getPath()).append("<br>");
+            sb.append("<p>Request method: ").append(request.getMethod()).append("<br>");
+
+
+            Map<String, String> args = request.getArguments();
+
+            if (!args.isEmpty()) {
+                sb.append("<p><strong>Passed arguments:</strong><br>");
+
+                for (Map.Entry<String, String> entry : args.entrySet()) {
+                    sb.append("Key: ").append(entry.getKey());
+                    sb.append(", Value: ").append(entry.getValue());
+                    sb.append("<br>");
+                }
+
+                sb.append("</p>");
+            }
+
+            Map<String, String> headers = request.getHeaders();
+
+            if (!headers.isEmpty()) {
+                sb.append("<p><strong>Passed headers:</strong><br>");
+
+                for (Map.Entry<String, String> entry : headers.entrySet()) {
+                    sb.append("Key: ").append(entry.getKey());
+                    sb.append(", Value: ").append(entry.getValue());
+                    sb.append("<br>");
+                }
+
+                sb.append("</p>");
+            }
+
+            sb.append(" <iframe width=\"420\" height=\"315\"\n" +
+                    "src=\"https://www.youtube.com/embed/dQw4w9WgXcQ\">\n" +
+                    "</iframe> ");
+
+            sb.append("</body></html>");
+
+            // Write everything to output
+            response.getOutputStream().write(sb.toString().getBytes());
+            response.getOutputStream().flush();
+        }
     }
 }
