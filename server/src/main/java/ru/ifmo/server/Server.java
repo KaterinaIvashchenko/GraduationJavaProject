4b7bd03e1e967ceb6a3bd52d2b0cd396a925044b
FedorovArs <Fedorov-Ars@yandex.ru>
2017-01-16 20:47:47 +0300

    small fix

--- server/src/main/java/ru/ifmo/server/Server.java
+++ server/src/main/java/ru/ifmo/server/Server.java
@@ -1,28 +1,30 @@
-package ru.ifmo.server;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import ru.ifmo.server.util.Utils;
+        package ru.ifmo.server;
 
-import java.io.Closeable;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.URISyntaxException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+        import org.slf4j.Logger;
+        import org.slf4j.LoggerFactory;
+        import ru.ifmo.server.util.Utils;
 
-import static ru.ifmo.server.Http.*;
+        import java.io.Closeable;
+        import java.io.IOException;
+        import java.io.OutputStream;
+        import java.net.ServerSocket;
+        import java.net.Socket;
+        import java.net.URISyntaxException;
+        import java.util.concurrent.ExecutorService;
+        import java.util.concurrent.Executors;
+
+        import static ru.ifmo.server.Http.*;
+        import static ru.ifmo.server.util.Utils.htmlMessage;
 
 /**
  * Ifmo Web Server.
  * <p>
- * To start server use {@link #start(ServerConfig)} and register at least
- * one handler to process HTTP requests.
- * Usage example:
- * <pre>
- * {@code
+ *     To start server use {@link #start(ServerConfig)} and register at least
+ *     one handler to process HTTP requests.
+ *     Usage example:
+ *     <pre>
+ *{@code
  * ServerConfig config = new ServerConfig()
  *      .addHandler("/index", new Handler() {
  *          public void handle(Request request, Response response) throws Exception {
@@ -37,9 +39,8 @@ import static ru.ifmo.server.Http.*;
  *     </pre>
  * </p>
  * <p>
- * To stop the server use {@link #stop()} or {@link #close()} methods.
+ *     To stop the server use {@link #stop()} or {@link #close()} methods.
  * </p>
- *
  * @see ServerConfig
  */
 public class Server implements Closeable {
@@ -50,10 +51,9 @@ public class Server implements Closeable {
 
     private final ServerConfig config;
 
-    private ServerSocket serverSocket;
+    private ServerSocket socket;
 
     private ExecutorService acceptorPool;
-    private ExecutorService connectionProcessingPool;
 
     private static final Logger LOG = LoggerFactory.getLogger(Server.class);
 
@@ -80,11 +80,12 @@ public class Server implements Closeable {
             Server server = new Server(config);
 
             server.openConnection();
-            server.startWorkers();
+            server.startAcceptor();
 
             LOG.info("Server started on port: {}", config.getPort());
             return server;
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             throw new ServerException("Cannot start server on port: " + config.getPort());
         }
     }
@@ -97,7 +98,7 @@ public class Server implements Closeable {
 
         if (statusCode == 0)
             statusCode = SC_OK;
-            response.setStatusCode(statusCode);
+        response.setStatusCode(statusCode);
 
         try {
             if (response.printWriter != null)
@@ -129,12 +130,11 @@ public class Server implements Closeable {
     }
 
     private void openConnection() throws IOException {
-        serverSocket = new ServerSocket(config.getPort());
+        socket = new ServerSocket(config.getPort());
     }
 
-    private void startWorkers() {
+    private void startAcceptor() {
         acceptorPool = Executors.newSingleThreadExecutor(new ServerThreadFactory("con-acceptor"));
-        connectionProcessingPool = Executors.newCachedThreadPool(new ServerThreadFactory("con-processor"));
 
         acceptorPool.submit(new ConnectionHandler());
     }
@@ -144,12 +144,10 @@ public class Server implements Closeable {
      */
     public void stop() {
         acceptorPool.shutdownNow();
-        connectionProcessingPool.shutdownNow();
-        Utils.closeQuiet(serverSocket);
+        Utils.closeQuiet(socket);
 
-        serverSocket = null;
+        socket = null;
     }
-
 
     private void processConnection(Socket sock) throws IOException {
         if (LOG.isDebugEnabled())
@@ -164,7 +162,8 @@ public class Server implements Closeable {
 
             if (LOG.isDebugEnabled())
                 LOG.debug("Parsed request: {}", req);
-        } catch (URISyntaxException e) {
+        }
+        catch (URISyntaxException e) {
             if (LOG.isDebugEnabled())
                 LOG.error("Malformed URL", e);
 
@@ -172,7 +171,8 @@ public class Server implements Closeable {
                     sock.getOutputStream());
 
             return;
-        } catch (Exception e) {
+        }
+        catch (Exception e) {
             LOG.error("Error parsing request", e);
 
             respond(SC_SERVER_ERROR, "Server Error", htmlMessage(SC_SERVER_ERROR + " Server error"),
@@ -199,126 +199,20 @@ public class Server implements Closeable {
         if (handler != null) {
             try {
                 handler.handle(req, resp);
+                flushResponse(resp);
             }
             catch (Exception e) {
                 if (LOG.isDebugEnabled())
                     LOG.error("Server error:", e);
                 respond(SC_SERVER_ERROR,htmlMessage(SC_SERVER_ERROR + " Server error"),resp);
             }
-        } else
+        }
+        else
             respond(SC_NOT_FOUND, "Not Found", htmlMessage(SC_NOT_FOUND + " Not found"),
                     sock.getOutputStream());
     }
 
-    private Request parseRequest(Socket socket) throws IOException, URISyntaxException {
-        InputStreamReader reader = new InputStreamReader(socket.getInputStream());
-
-        Request req = new Request(socket);
-        StringBuilder sb = new StringBuilder(READER_BUF_SIZE); // TODO
-
-        while (readLine(reader, sb) > 0) {
-            if (req.method == null)
-                parseRequestLine(req, sb);
-            else
-                parseHeader(req, sb);
-
-            sb.setLength(0);
-        }
-
-        return req;
-    }
-
-    private void parseRequestLine(Request req, StringBuilder sb) throws URISyntaxException {
-        int start = 0;
-        int len = sb.length();
-
-        for (int i = 0; i < len; i++) {
-            if (sb.charAt(i) == SPACE) {
-                if (req.method == null)
-                    req.method = HttpMethod.valueOf(sb.substring(start, i));
-                else if (req.path == null) {
-                    req.path = new URI(sb.substring(start, i));
-
-                    break; // Ignore protocol for now
-                }
-
-                start = i + 1;
-            }
-        }
-
-        assert req.method != null : "Request method can't be null";
-        assert req.path != null : "Request path can't be null";
-
-        String query = req.path.getQuery();
-
-        if (query != null) {
-            start = 0;
-
-            String key = null;
-
-            for (int i = 0; i < query.length(); i++) {
-                boolean last = i == query.length() - 1;
-
-                if (key == null && query.charAt(i) == EQ) {
-                    key = query.substring(start, i);
-
-                    start = i + 1;
-                }
-                else if (key != null && (query.charAt(i) == AMP || last)) {
-                    req.addArgument(key, query.substring(start, last ? i + 1 : i));
-
-                    key = null;
-                    start = i + 1;
-                }
-            }
-
-            if (key != null)
-                req.addArgument(key, null);
-        }
-    }
-
-    private void parseHeader(Request req, StringBuilder sb) {
-        String key = null;
-
-        int len = sb.length();
-        int start = 0;
-
-        for (int i = 0; i < len; i++) {
-            if (sb.charAt(i) == HEADER_VALUE_SEPARATOR) {
-                key = sb.substring(start, i).trim();
-
-                start = i + 1;
-
-                break;
-            }
-        }
-
-        req.addHeader(key, sb.substring(start, len).trim());
-    }
-
-    private int readLine(InputStreamReader in, StringBuilder sb) throws IOException {
-        int c;
-        int count = 0;
-
-        while ((c = in.read()) >= 0) {
-            if (c == LF)
-                break;
-
-            sb.append((char) c);
-
-            count++;
-        }
-
-        if (count > 0 && sb.charAt(count - 1) == CR)
-            sb.setLength(--count);
-
-        if (LOG.isTraceEnabled())
-            LOG.trace("Read line: {}", sb.toString());
-
-        return count;
-    }
-
-    private void respond(int code, String statusMsg, String content, OutputStream out) throws IOException {
+    static void respond(int code, String statusMsg, String content, OutputStream out) throws IOException {
         out.write(("HTTP/1.0" + SPACE + code + SPACE + statusMsg + CRLF + CRLF + content).getBytes());
         out.flush();
     }
@@ -354,37 +248,15 @@ public class Server implements Closeable {
     private class ConnectionHandler implements Runnable {
         public void run() {
             while (!Thread.currentThread().isInterrupted()) {
-                try {
-                    Socket sock = serverSocket.accept();
+                try (Socket sock = socket.accept()) {
                     sock.setSoTimeout(config.getSocketTimeout());
 
-                     connectionProcessingPool.submit(new RequestProcessor(sock));
-                } catch (Exception e) {
+                    processConnection(sock);
+                }
+                catch (Exception e) {
                     if (!Thread.currentThread().isInterrupted())
                         LOG.error("Error accepting connection", e);
                 }
-            }
-
-        }
-    }
-
-    private class RequestProcessor implements Runnable {
-        Socket sock;
-
-        RequestProcessor(Socket sock) {
-            this.sock = sock;
-        }
-
-        @Override
-        public void run() {
-            try {
-                processConnection(sock);
-            }
-            catch (IOException e) {
-                LOG.error("Error input / output during data transfer", e);
-            }
-            finally {
-                closeQuiet(sock);
             }
         }
     }
