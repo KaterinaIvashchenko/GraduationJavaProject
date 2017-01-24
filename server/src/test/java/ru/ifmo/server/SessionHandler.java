3384d25d0ac96ee8b134dd492d329c7c96390878
GilEO <gil707@gmail.com>
2017-01-24 15:51:31 +0300

    Session v7

--- server/src/test/java/ru/ifmo/server/SessionHandler.java
+++ server/src/test/java/ru/ifmo/server/SessionHandler.java
@@ -8,7 +8,7 @@ public class SessionHandler implements Handler {
     @Override
     public void handle(Request request, Response response) throws Exception {
         response.setCookie(new Cookie("VKJ", "name"));
-        response.setBody(request.getHeaders().get("Cookie").getBytes());
+
 //        request.getSession();
     }
 }
